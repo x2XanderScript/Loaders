294b736f2d2a1aa31711dabce322c2f89fce6ee9
x2XanderScript <thnchotiewengwrrn@gmail.com>
2026-01-11 02:15:15 -0800

    Update RGP.lua

--- Maps/RGP.lua
+++ Maps/RGP.lua
@@ -1,1 +1,13 @@
-print("")
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "My Super Hub",
+    Icon = "door-open", -- lucide icon. optional
+    Author = "by .ftgs and .ftgs", -- optional
+})
+
+local Tab = Window:Tab({
+    Title = "Tab Title",
+    Icon = "bird", -- optional
+    Locked = false,
+})

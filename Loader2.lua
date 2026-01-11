35688e18008d790b0f659e5f1d492c723799d595
x2XanderScript <thnchotiewengwrrn@gmail.com>
2026-01-11 02:09:27 -0800

    Update Loader2.lua

--- Loader2.lua
+++ Loader2.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [84988808589910] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua"
+  [84988808589910] = "https://raw.githubusercontent.com/x2XanderScript/Loaders/refs/heads/main/Maps/RGP.lua"
 }
 
 return Games
